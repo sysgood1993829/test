ffc43f6f784a927f6dc8741f8ad51946fb36b4f2
sysgood1993829 <b00901092@ntu.edu.tw>
2015-08-10 18:06:14 +0800

    first commit

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : eric_hsiao
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout<< "GG~~~~" <<endl;
+	int n1,n2;
+	cout << "Please enter two number:"<<endl;
+	cin >> n1;
+	cin >> n2;
+	cout << "Their sum is: "<<n1+n2;
+	return 0;
+}
